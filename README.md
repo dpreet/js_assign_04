057b56d60b56cfb485a6c30f758dc715fca8c12e
Dilpreet Sanghera <dilpreetk000@gmail.com>
2017-03-16 06:33:08 +0000

    first commit

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # INTRODUCTION
 ------------------------
-This is a todoList app which do the following functions:
-1. Add an item to the list
-2. Delete an item from the list
-3. Marks an item as completed
-4. Resets an item if it needs to be done again
+This is a todoList app which do the following functions: /n
+1. Add an item to the list/n
+2. Delete an item from the list/n
+3. Marks an item as completed/n
+4. Resets an item if it needs to be done again /n
+
+## USING LOCAL STORAGE 
+---------------------------
+In this app we need to put each item in local storage in order to retrieve those items for future use.
 
 
 
--- app.js
+++ app.js
@@ -1,52 +1,106 @@
-
+const ls = localStorage;
 const btn = document.getElementById('addTodo');
-btn.addEventListener('click', (event) => {
 
-  event.preventDefault();
-  const addItem = document.getElementById('addTodoItem').value;
-  const newList = document.createElement('li');
-  newList.textContent=addItem;
-  document.getElementById('todoList').appendChild(newList);
-  
-  var delBtn = document.createElement('button');
-  delBtn.textContent = 'delete';
-  newList.appendChild(delBtn);
+// ls.clear();
 
-delBtn.addEventListener('click' , (event) => {
 
-            event.preventDefault();
+function get_list(){
+
+			let arr = new Array() ;
+			arr = ls.getItem('key');
+			if(arr!=null){
+
+				arr= JSON.parse(arr);
+			}
+			return arr ;
+}
+
+function addToStorage(addItem){
+
+			 let arr = get_list() || [];
+			 arr.push(addItem);
+			 arr.push('unmark') ;
+			 ls.setItem('key' , JSON.stringify(arr));
+			  display();
             
-            newList.remove();
+}
+
+
+btn.addEventListener('click', (add) => { 
+  add.preventDefault();
+  // const newList = document.createElement('li');
+  document.getElementById('addTodoItem').innerHTML = '';
+  let addItem = document.getElementById('addTodoItem').value;
+   addToStorage(addItem);
+
+  
 
 });
+ 
+ function display(){
 
-var click = 0;
-newList.addEventListener('click', (event) => {
+ 				let arr = new Array();
+ 				 arr = get_list() || [] ;
+ 				const list = document.getElementById('todoList');
+ 				list.innerHTML = '';
+ 				for ( let i = 0 ; i < arr.length ; i=i+2 ){
 
-event.preventDefault();
+ 					const newList = document.createElement('li');
+ 					newList.textContent = arr[i];
+ 					newList.className = arr[i+1];
+ 					let delBtn = document.createElement('button');
+  					delBtn.textContent = 'delete';
 
-if(click%2==0){
-newList.className = "strike";
+  					list.appendChild(newList);
+  					newList.appendChild(delBtn);
+
+                    delBtn.addEventListener('click' , (removeFromStorage));
+                    newList.addEventListener('click' , (mark));
+
+            	}
+					
+ 				}
+
+ 
+
+ function removeFromStorage(){
+
+
+ 	let id = this.getAttribute('id');
+    let todos = get_list();
+    todos.splice(id, 2);
+    ls.setItem('key', JSON.stringify(todos));
+ 
+    display();  
+ }
+ 
+ function mark(){
+
+
+ 	if (this.className === 'unmark' ){
+
+ 		this.className = 'mark';
+ 		let id = this.getAttribute('id');
+ 		let todos = get_list();
+ 		todos[id+1] = 'mark';
+        ls.setItem('key', JSON.stringify(todos));
+
+
+ 	}
+
+ 	else{
+
+ 		this.className = 'unmark' ;
+ 		let id = this.getAttribute('id');
+ 		let todos = get_list();
+ 		todos[id+1] = 'unmark';
+        ls.setItem('key', JSON.stringify(todos));
+ 	}
+ 					
+
 
 }
 
-else{
-
-newList.className = "strike2";
-
-}
-
-click++;
-
-
-
-});
-
-
-
-}); 
-
-
-		
+ display();
 
 
